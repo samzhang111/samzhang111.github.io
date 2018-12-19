e10e562e7ab6097656357e4f64b7a8675440874c
samzhang111 <shimian.zhang@gmail.com>
2018-12-19 00:22:31 -0700

    cat post

---
+++ _posts/2018-12-18-cats-on-ladders.md
@@ -0,0 +1,40 @@
+---
+layout: post
+title: Cats on ladders
+date: 2018-12-18 12:00:00
+tag: geometry
+scripts:
+    - "https://cdnjs.cloudflare.com/ajax/libs/jsxgraph/0.99.7/jsxgraphcore.js"
+    - "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js"
+    - "/scripts/cat.js"
+styles:
+    - "https://cdnjs.cloudflare.com/ajax/libs/jsxgraph/0.99.7/jsxgraph.css"
+---
+
+**Problem 1**. Suppose a ladder is leaning vertically against a wall, and a cat is sitting on the midpoint of the ladder. If the base of the ladder begins to slide away from the wall with the top of the ladder maintaining contact with the wall, what shape is traced by the path of the cat?
+
+
+It goes without saying that you should try to formulate an answer before playing with the widget. Drag the "Ladder" slider to cause the ladder to move.
+
+<div id="solocat" class="jxgbox" style="width:500px; height:500px; margin-left:auto; margin-right:auto;">
+</div>
+
+Maybe the presence of the "Cat" slider gives away this second problem:
+
+**Problem 2**. Suppose the cat is again on the ladder, but this time, not at the midpoint. What path does it trace now? (Use the "Cat" slider to move the cat around.)
+
+Here is a reflected version of the same visualization, in case you want to see four ladders going all at once.
+
+<div id="multicat" class="jxgbox" style="width:500px; height:500px; margin-left:auto; margin-right:auto;">
+</div>
+
+
+Those are the first two problems in this wonderful little geometry book:
+
+<a href="https://www.springer.com/la/book/9780817641610"><img src="/images/linesandcurves.jpg?raw=true" alt="Cover of Lines and Curves"/></a>
+
+It was translated from Russian, and originally used in the Gelfand Correspondence School. The perspective of the book is to explore basic geometry from the perspective of "motion": ladders sliding with cats sitting on them, little rings attached to circles being pulled on strings, etc.
+
+That perspective lends itself very nicely to interactive computer exploration, and I am tempted to digitize more of these problems for fun...
+
+I used the library [JSXGraph](https://jsxgraph.org/). For my source code, see [here](https://github.com/samzhang111/linesandcurves).
---
+++ scripts/cat.js
@@ -0,0 +1,150 @@
+(() => {
+    const xmax = 1.5;
+    const ymax = 1.5;
+    const numpoints = 10;
+
+    JXG.Options.layer['polygon'] = 8;
+    const board = JXG.JSXGraph.initBoard('multicat', {
+        boundingbox: [-xmax, xmax, ymax, -ymax],
+        axis: true
+    });
+
+    let slider = board.create('slider', [[0.75,1],[1.25,1],[0,0,1]],{name:'Ladder'});
+    let catslider = board.create('slider', [[0.75,1.25],[1.25,1.25],[0,0.5,1]],{name:'Cat'});
+
+    catslider.on('drag', function() {
+        reset();
+        cat1.moveTo([0, catslider.Value()])
+        cat2.moveTo([0, catslider.Value()])
+        cat3.moveTo([0, -catslider.Value()])
+        cat4.moveTo([0, -catslider.Value()])
+    })
+
+    let ladder1 = board.create('curve', [
+        function(t) { 
+            let x = t * slider.Value(); 
+
+            if (x >= 0 && x <= 1) {
+                return x;
+            }
+        },
+        function(t) { 
+            let y = (1 - t) * Math.sqrt(1 - slider.Value() * slider.Value()); 
+
+            if (y >= 0 && y <= 1) {
+                return y;
+            }
+        }
+        ], { strokewidth: 4});
+    var cat1 = board.create('glider', [0, 0.5, ladder1], {trace: true, name: 'Cat', fixed: true})
+
+    let ladder2 = board.create('curve', [
+        function(t) { 
+            let x = t * slider.Value(); 
+
+            if (x >= 0 && x <= 1) {
+                return -x;
+            }
+        },
+        function(t) { 
+            let y = (1 - t) * Math.sqrt(1 - slider.Value() * slider.Value()); 
+
+            if (y >= 0 && y <= 1) {
+                return y;
+            }
+        }
+        ], { strokewidth: 4});
+    var cat2 = board.create('glider', [0, 0.5, ladder2], {trace: true, name: 'Cat', fixed: true})
+
+    let ladder3 = board.create('curve', [
+        function(t) { 
+            let x = t * slider.Value(); 
+
+            if (x >= 0 && x <= 1) {
+                return -x;
+            }
+        },
+        function(t) { 
+            let y = (1 - t) * Math.sqrt(1 - slider.Value() * slider.Value()); 
+
+            if (y >= 0 && y <= 1) {
+                return -y;
+            }
+        }
+        ], { strokewidth: 4});
+    var cat3 = board.create('glider', [0, -0.5, ladder3], {trace: true, name: 'Cat', fixed: true})
+
+    let ladder4 = board.create('curve', [
+        function(t) { 
+            let x = t * slider.Value(); 
+
+            if (x >= 0 && x <= 1) {
+                return x;
+            }
+        },
+        function(t) { 
+            let y = (1 - t) * Math.sqrt(1 - slider.Value() * slider.Value()); 
+
+            if (y >= 0 && y <= 1) {
+                return -y;
+            }
+        }
+        ], { strokewidth: 4});
+    var cat4 = board.create('glider', [0, -0.5, ladder4], {trace: true, name: 'Cat', fixed: true})
+
+    function reset() {
+        cat1.clearTrace();
+        cat2.clearTrace();
+        cat3.clearTrace();
+        cat4.clearTrace();
+        slider.setValue(0);
+    }
+
+    var resetbutton = board.create('button', [0.75, 0.75, "Reset path", reset])
+})();
+
+( () => {
+    const xmax = 1.5;
+    const ymax = 1.5;
+    const numpoints = 10;
+
+    JXG.Options.layer['polygon'] = 8;
+    const board = JXG.JSXGraph.initBoard('solocat', {
+        boundingbox: [-xmax/5, xmax, ymax, -ymax/5],
+        axis: true
+    });
+
+    let slider = board.create('slider', [[0.75,1],[1.25,1],[0,0,1]],{name:'Ladder'});
+    let catslider = board.create('slider', [[0.75,1.25],[1.25,1.25],[0,0.5,1]],{name:'Cat'});
+
+    catslider.on('drag', function() {
+        reset();
+        cat1.moveTo([0, catslider.Value()])
+    })
+
+    let ladder1 = board.create('curve', [
+        function(t) { 
+            let x = t * slider.Value(); 
+
+            if (x >= 0 && x <= 1) {
+                return x;
+            }
+        },
+        function(t) { 
+            let y = (1 - t) * Math.sqrt(1 - slider.Value() * slider.Value()); 
+
+            if (y >= 0 && y <= 1) {
+                return y;
+            }
+        }
+        ], { strokewidth: 4});
+    var cat1 = board.create('glider', [0, 0.5, ladder1], {trace: true, name: 'Cat', fixed: true})
+
+
+    function reset() {
+        cat1.clearTrace();
+        slider.setValue(0);
+    }
+
+    var resetbutton = board.create('button', [0.75, 0.75, "Reset path", reset])
+})();
