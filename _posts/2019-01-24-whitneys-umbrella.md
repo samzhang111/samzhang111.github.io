921fca083063deac0b737f480b67cd585d00c36f
samzhang111 <shimian.zhang@gmail.com>
2019-01-24 12:06:34 -0700

    whitney and gallery

---
+++ _posts/2019-01-24-whitneys-umbrella.md
@@ -0,0 +1,21 @@
+---
+layout: post
+title: Whitney's Umbrella
+date: 2019-01-24 12:00:00
+tag: geometry
+scripts:
+    - "/scripts/mathbox-bundle.min.js"
+    - "/scripts/umbrella.js"
+styles:
+    - "/css/mathbox.css"
+---
+
+I've been test driving the excellent [mathbox](https://github.com/unconed/mathbox/) WebGL library. For starters, here's a pan-and-zoomable [Whitney's umbrella](https://en.wikipedia.org/wiki/Whitney_umbrella):
+
+<div id="umbrella" style="width:600px;height:600px;margin-right:auto"></div>
+
+[Full screen version](/html/umbrella). 
+
+I have been thinking about how to organize the contents of this blog more visually as well. Thus I have created [an early version of a "gallery" concept](/html/gallery).
+
+My main inspirations for beautiful mathematical illustrations have been [Geometry and the Imagination](https://www.maa.org/press/maa-reviews/geometry-and-the-imagination) and [A Topological Picturebook](https://www.springer.com/us/book/9780387345420). As I become more comfortable with mathbox, and (painfully) shake the rust off of my d3 skills (while learning d3 v4), I hope more of the images from those books make their way to this blog.
---
+++ scripts/umbrella.js
@@ -0,0 +1,105 @@
+var mathbox = mathBox({
+  element: document.querySelector("#umbrella"),
+  plugins: ['core', 'controls', 'cursor', 'mathbox'],
+  controls: {
+    // Orbit controls, i.e. Euler angles, with gimbal lock
+    klass: THREE.OrbitControls,
+
+    // Trackball controls, i.e. Free quaternion rotation
+    //klass: THREE.TrackballControls,
+  },
+  renderer: { parameters: { alpha: true } }
+});
+if (mathbox.fallback) throw "WebGL not supported"
+
+var three = mathbox.three;
+three.renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
+three.renderer.setClearAlpha(0);
+
+var camera = mathbox.camera({
+    proxy: true,
+    position: [-1, -1, -3]
+})
+mathbox.set('focus', 1);
+
+let colors = {
+    x: new THREE.Color(0xFF4136),
+    y: new THREE.Color(0x2ECC40),
+    z: new THREE.Color(0x0074D9),
+};
+
+let size = 2;
+let xmin = ymin = zmin = -size;
+let xmax = ymax = zmax = size;
+
+var view = mathbox.cartesian({
+  range: [[xmin, xmax], [ymin, ymax], [zmin, zmax]],
+  scale: [1, 1, 1],
+});
+
+let sampler = view.area({
+  id: 'sampler',
+  width: 64,
+  height: 64,
+  axes: [1, 3],
+  expr: function (emit, x, y, i, j, time) {
+    emit(x*y, x, y*y - 2)
+  },
+  live: false,
+  items: 1,
+  channels: 3,
+});
+
+let color=    view.volume({
+      id: "color",
+      width: 64,
+      height: 64,
+      items: 1,
+      channels: 4,
+      expr: function(emit, x, y, z){
+          let color = y*y/4
+          let color2 = (x*y + 4)/8
+          emit(color2, color, 0, 0.75);
+    },
+    live: false
+    })
+
+    /*
+let xlim = 4, ylim = 2, zlim = 2;
+
+let faces = view.voxel({
+    id: 'faces',
+    items: 17,
+    channels: 3,
+    data: [[-xlim, ylim, zlim], [xlim, ylim, zlim],
+           [xlim, -ylim, zlim], [-xlim, -ylim, zlim], [-xlim, ylim, zlim],
+           [-xlim, ylim, -zlim], [-xlim, -ylim, -zlim], [-xlim, -ylim, zlim],
+           [-xlim, -ylim, -zlim], [xlim, -ylim, -zlim],
+            [xlim, -ylim, zlim], [xlim, -ylim, -zlim],
+            [xlim, ylim, -zlim], [xlim, ylim, zlim], [xlim, ylim, -zlim],
+            [-xlim, ylim, -zlim], [-xlim, -ylim, -zlim], 
+    ],
+    live: false
+})
+
+view.face({
+    points: faces,
+    shaded: false,
+    line: true,
+    fill: false,
+    width: 5
+})
+    */
+
+view.surface({
+  shaded: true,
+  lineX: true,
+  lineY: true,
+  points: sampler,
+  colors: color,
+  color: 0xFFFFFF,
+  width: 5,
+  fill: true
+});
+
+
